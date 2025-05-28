a8eb2dda719f9eb33f48dd01bcabec254823665f
Zach Latta <zach@zachlatta.com>
2025-05-28 11:21:18 -0400

    Sync The Bay and Journey

--- orpheus_engine/defs/sling/definitions.py
+++ orpheus_engine/defs/sling/definitions.py
@@ -1,10 +1,12 @@
 from dagster import Definitions
-from .assets import hackatime_sling_assets, hcer_public_github_data_sling_assets, sling_replication_resource
+from .assets import hackatime_sling_assets, hcer_public_github_data_sling_assets, journey_sling_assets, shipwrecked_the_bay_sling_assets, sling_replication_resource
 
 defs = Definitions(
     assets=[
         hackatime_sling_assets,
         hcer_public_github_data_sling_assets,
+        journey_sling_assets,
+        shipwrecked_the_bay_sling_assets,
     ],
     resources={
         "sling": sling_replication_resource,
