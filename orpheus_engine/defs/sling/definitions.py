168b4d4fb967dd2590bbd8836ea2e3e403928e60
Zach Latta <zach@zachlatta.com>
2025-06-24 16:34:29 -0400

    Register Summer of Making 2025 asset in Sling definitions
    - Add summer_of_making_2025_warehouse_mirror to import statement
    - Add asset to Definitions assets list
    - Fixes asset visibility in Dagster UI

--- orpheus_engine/defs/sling/definitions.py
+++ orpheus_engine/defs/sling/definitions.py
@@ -4,6 +4,7 @@ from .assets import (
     hcer_public_github_data_warehouse_mirror,
     journey_warehouse_mirror,
     shipwrecked_the_bay_warehouse_mirror,
+    summer_of_making_2025_warehouse_mirror,
     sling_replication_resource,
 )
 
@@ -13,6 +14,7 @@ defs = Definitions(
         hcer_public_github_data_warehouse_mirror,
         journey_warehouse_mirror,
         shipwrecked_the_bay_warehouse_mirror,
+        summer_of_making_2025_warehouse_mirror,
     ],
     resources={
         "sling": sling_replication_resource,
